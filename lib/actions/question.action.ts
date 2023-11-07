97579e331632901f4534d04b23a2a92f5c4d4b7d
asdasdwasd6699 <aldialamsyah911@gmail.com>
2023-11-07 21:24:37 +0700

    idk why my question didnt submited

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,7 +15,6 @@ export async function getQuestions(params: GetQuestionsParams) {
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
       .sort({ createdAt: -1 });
-
     return { questions };
   } catch (error) {
     console.log("====================================");
